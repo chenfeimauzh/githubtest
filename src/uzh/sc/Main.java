17beb71c149c186173d4070de1552c4c9d8d607a
chenfei ma <91160241+chenfeimauzh@users.noreply.github.com>
2022-03-28 16:32:49 +0200

    master branch first commit

--- src/uzh/sc/Main.java
+++ src/uzh/sc/Main.java
@@ -8,6 +8,15 @@ public class Main {
         // baseline code
 
 
+
+
+        //masterbranch code 1
+        //masterbranch code 2
+        //masterbranch code 3
+        //masterbranch code 4
+
+
+
         //us1 branch code 1
         //us1 branch code 2
         //us1 branch code 3
